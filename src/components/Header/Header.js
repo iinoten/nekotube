4420361439119fd78f08afaa28b930f1ee4ef3bf
Iino Ten <nteten0313@icloud.com>
2018-11-18 07:28:59 +0900

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render(){  
+    return(
+      <div style={{textAlign: 'center'}}>
+        <div>
+          <h2>NekoTube</h2>
+        </div>
+        <div>
+          <input />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Header;
