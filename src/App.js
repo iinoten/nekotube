e8ae9dad2f38e1d69086b4ac577d960a915fd9ed
Iino Ten <nteten0313@icloud.com>
2018-11-18 15:07:33 +0900

    いろいろ

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import './App.css';
 import Header from './components/Header/Header'
 
+const YOUTUBE_API_KEY = "AIzaSyAxg3h8QHgEx1Hr034KSkWk8MD8mAxFTk8";
+
 class App extends Component {
   render() {
     return (
