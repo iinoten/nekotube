503785552b24cf8a8975101763dc2e1d9c77028f
Iino Ten <nteten0313@icloud.com>
2018-11-19 03:51:35 +0900

    動画アイテムをmap関数で繰り返し表示させた

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 import React, { Component } from 'react'
 import './App.css';
 import Header from './components/Header/Header'
+import Body from './components/Body/Body'
+import List from './components/Video/List/List'
 
+import YSearch from '../node_modules/youtube-api-search';
+
+const YOUTUBE_API_KEY = 'AIzaSyAxg3h8QHgEx1Hr034KSkWk8MD8mAxFTk8'
 
 class App extends Component {
+  state = {videos: []}
+
+  componentDidMount() {
+    YSearch({ key: YOUTUBE_API_KEY, term: '猫　きゅうり'}, (data) => {
+      this.setState({videos: data})  
+    });　
+  }
+
   render() {
     return (
       <div className="App">
         <Header />
+
+        <Body><List videos={this.state.videos} /></Body>
       </div>
     );
   }
 }
-export default App
+export default App;
---
+++ src/components/Video/Item/Item.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Item = (props) => {
+    return (
+        <li>動画のアイテム</li>
+    );
+};
+
+export default Item;
